0ea06f25c8ec56bcb1c6e942d23796ea03a08ef0
Sanith Bimsara <05sanith@gmail.com>
2025-09-02 20:22:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -295,7 +295,21 @@ def get_size(size):
         i += 1
         size /= 1024.0
     return "%.2f %s" % (size, units[i])
+    
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "d"]
 
+    while count < 4:
+        count += 1
+        if seconds == 0:
+            break
+        seconds, remainder = divmod(seconds, 60 if count < 3 else 24)
+        time_list.append(f"{int(remainder)}{time_suffix_list[count-1]}")
+
+    return " ".join(reversed(time_list))
+    
 def silent_size(size):
     size = float(size)
     size_gb = size / (1024 ** 3)
